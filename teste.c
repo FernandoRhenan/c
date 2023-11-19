948c897100967517d308c831a4feb1f884d937a1
FernandoRhenan <fernandorhenan9@gmail.com>
2023-11-19 19:29:25 -0300

    first

---
+++ teste.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int a, b;	
+		
+	scanf("%d", &a);
+	scanf("%d", &b);
+	
+	printf("Soma: %d", a + b);
+	printf("Subtra: %d", a - b);
+	printf("Multiplica: %d", a * b);
+	printf("Divi: %d", a / b);
+		
+	system("pause");
+	
+}
+
